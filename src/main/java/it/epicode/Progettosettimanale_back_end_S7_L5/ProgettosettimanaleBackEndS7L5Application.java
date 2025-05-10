c1a2a672a69adf1ac4e7d30f1e6fe417bd006b35
GiorgiaMannino <giorgimannino@hotmail.com>
2025-05-10 10:28:27 +0200

    creazione

---
+++ src/main/java/it/epicode/Progettosettimanale_back_end_S7_L5/ProgettosettimanaleBackEndS7L5Application.java
@@ -0,0 +1,13 @@
+package it.epicode.Progettosettimanale_back_end_S7_L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettosettimanaleBackEndS7L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettosettimanaleBackEndS7L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/Progettosettimanale_back_end_S7_L5/ProgettosettimanaleBackEndS7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Progettosettimanale_back_end_S7_L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettosettimanaleBackEndS7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
